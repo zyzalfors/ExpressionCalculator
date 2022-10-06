093f4ba743f20f9b639ac5965aa945025827d410
zyzalfors <55386215+zyzalfors@users.noreply.github.com>
2022-10-06 21:10:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Data;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Calc
+{
+	
+    internal static class Calc
+    {
+        private static DataTable table;
+
+        private static readonly string[] operations = {
+                                                       "Pi",
+                                                       "*", 
+                                                       "/", 
+                                                       "+",
+                                                       "-", 
+                                                       "Abs", 
+                                                       "Acos",
+                                                       "Acosh",
+                                                       "Asin",
+                                                       "Asinh",
+                                                       "Atan",
+                                                       "Atan2",   
+                                                       "Atanh",
+                                                       "Cbrt",
+                                                       "Ceil",
+                                                       "Cos",
+                                                       "Cosh",
+                                                       "Exp",
+                                                       "Floor",
+                                                       "Log",
+                                                       "Ln",
+                                                       "Log10",
+                                                       "Log2",
+                                                       "Pow",
+                                                       "Sign",
+                                                       "Sin",
+                                                       "Sinh",
+                                                       "Sqrt",
+                                                       "Tan",
+                                                       "Tanh"
+                                                      };
+
+        private static readonly string pattern = @"[+-]?([0-9]+\.?[0-9]*|\.[0-9]+)(E-([0-9]+))?";
+
+        private static string Digest(string expression)
+        {
+            string digestedExpression = expression.Replace(" ", string.Empty);
+            string oldDigestedExpression = string.Empty;
+            while(digestedExpression != oldDigestedExpression)
+            {
+                oldDigestedExpression = digestedExpression;
+                foreach(string operation in operations)
+                {
+                    if(operation == "Pi")
+                    { 
+                        foreach(Match match in Regex.Matches(digestedExpression, operation, RegexOptions.IgnoreCase))
+                        {
+                            digestedExpression = digestedExpression.Replace(match.Value, CalculateOperation(operation).ToString());
+                        }
+                    }
+                    else if(operation == "*")
+                    {
+                        foreach(Match match in Regex.Matches(digestedExpression, pattern + @"\*" + pattern, RegexOptions.IgnoreCase))
+                        {
+                            digestedExpression = digestedExpression.Replace(match.Value, "+" + CalculateExpression(match.Value));
+                        }
+                    }
+                    else if(operation == "/")
+                    {
+                        foreach(Match match in Regex.Matches(digestedExpression, pattern + "/" + pattern, RegexOptions.IgnoreCase))
+                        {
+                            digestedExpression = digestedExpression.Replace(match.Value, "+" + CalculateExpression(match.Value));
+                        }
+                    }
+                    else if(operation == "+")
+                    {
+                        foreach(Match match in Regex.Matches(digestedExpression, pattern + @"\+" + pattern, RegexOptions.IgnoreCase))
+                        {
+                            digestedExpression = digestedExpression.Replace(match.Value, "+" + CalculateExpression(match.Value));
+                        }
+                    }
+                    else if (operation == "-")
+                    {
+                        foreach(Match match in Regex.Matches(digestedExpression, pattern + "-" + pattern, RegexOptions.IgnoreCase))
+                        {
+                            digestedExpression = digestedExpression.Replace(match.Value, "+" + CalculateExpression(match.Value));
+                        }
+                    }
+                    else if(operation == "Pow" || operation == "Atan2")
+                    {
+                        foreach(Match globalMatch in Regex.Matches(digestedExpression, operation + @"\(" + pattern + "," + pattern + @"\)", RegexOptions.IgnoreCase))
+                        {
+                            foreach(Match numMatch in Regex.Matches(globalMatch.Value, pattern, RegexOptions.IgnoreCase))
+                            {
+                                if(numMatch.NextMatch().Value.Trim() == string.Empty) { continue; }
+                                digestedExpression = digestedExpression.Replace(globalMatch.Value, CalculateOperation(operation, Convert.ToDouble(numMatch.Value), Convert.ToDouble(numMatch.NextMatch().Value)).ToString());
+                            }
+                        }
+                    }
+                    else
+                    {
+                        foreach(Match globalMatch in Regex.Matches(digestedExpression, operation + @"\(" + pattern + @"\)", RegexOptions.IgnoreCase))
+                        {
+                            foreach(Match numMatch in Regex.Matches(globalMatch.Value, pattern, RegexOptions.IgnoreCase))
+                            {
+                                digestedExpression = digestedExpression.Replace(globalMatch.Value, CalculateOperation(operation, Convert.ToDouble(numMatch.Value)).ToString());
+                            }
+                        }
+                    }
+                }
+            }
+            return digestedExpression;
+        }
+
+        private static double CalculateOperation(string operation, double x = 1, double y = 1)
+        {
+            switch(operation)
+            {
+                case "+":
+                    return x + y;
+                case "-":
+                    return x - y;
+                case "*":
+                    return x * y;
+                case "/":
+                    return x / y;
+                case "Abs":
+                    return Math.Abs(x);
+                case "Acos":
+                    return Math.Acos(x);
+                case "Acosh":
+                    return Math.Acosh(x);
+                case "Asin":
+                    return Math.Asin(x);
+                case "Asinh":
+                    return Math.Asinh(x);
+                case "Atan":
+                    return Math.Atan(x);
+                case "Atan2":
+                    return Math.Atan2(x, y);
+                case "Atanh":
+                    return Math.Atanh(x);
+                case "Cbrt":
+                    return Math.Cbrt(x);
+                case "Ceil":
+                    return Math.Ceiling(x);
+                case "Cos":
+                    return Math.Cos(x);
+                case "Cosh":
+                    return Math.Cosh(x);
+                case "Exp":
+                    return Math.Exp(x);
+                case "Floor":
+                    return Math.Floor(x);
+                case "Log":
+                case "Ln":
+                    return Math.Log(x);
+                case "Log10":
+                    return Math.Log10(x);
+                case "Log2":
+                    return Math.Log2(x);
+                case "Pi":
+                    return Math.PI;
+                case "Pow":
+                    return Math.Pow(x, y);
+                case "Sign":
+                    return Math.Sign(x);
+                case "Sin":
+                    return Math.Sin(x);
+                case "Sinh":
+                    return Math.Sinh(x);
+                case "Sqrt":
+                    return Math.Sqrt(x);
+                case "Tan":
+                    return Math.Tan(x);
+                case "Tanh":
+                    return Math.Tanh(x);
+                default:
+                    return 0;
+            }
+        }
+
+        private static string CalculateExpression(string expression)
+        {
+            try
+            {
+				string result = table.Compute(expression, null).ToString();
+                return result;
+            }
+            catch
+            {
+                return "Invalid expression";
+            }        
+        }
+
+        private static void Main(string[] args)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
+            table = new DataTable();
+            if (args.Length == 1)
+            {
+                Console.WriteLine(CalculateExpression(Digest(args[0])));
+                return;
+            }            
+            Console.WriteLine("Expression Calculator v1.0.0");
+            while(true)
+            {
+                Console.Write("> ");
+                Console.WriteLine(CalculateExpression(Digest(Console.ReadLine())));
+            }
+        }
+    }
+}
---
+++ Program.vb
@@ -0,0 +1,176 @@
+Imports System
+Imports System.Data
+Imports System.Globalization
+Imports System.Text.RegularExpressions
+
+Module Program
+
+    ReadOnly Operations As String() = {
+                                       "Pi",
+                                       "*",
+                                       "/",
+                                       "+",
+                                       "-",
+                                       "Abs",
+                                       "Acos",
+                                       "Acosh",
+                                       "Asin",
+                                       "Asinh",
+                                       "Atan",
+                                       "Atan2",
+                                       "Atanh",
+                                       "Cbrt",
+                                       "Ceil",
+                                       "Cos",
+                                       "Cosh",
+                                       "Exp",
+                                       "Floor",
+                                       "Log",
+                                       "Ln",
+                                       "Log10",
+                                       "Log2",
+                                       "Pow",
+                                       "Sign",
+                                       "Sin",
+                                       "Sinh",
+                                       "Sqrt",
+                                       "Tan",
+                                       "Tanh"
+                                      }
+
+    ReadOnly Pattern As String = "[+-]?([0-9]+\.?[0-9]*|\.[0-9]+)(E-([0-9]+))?"
+
+    Function Digest(Expression As String) As String
+        Dim DigestedExpression As String = Expression.Replace(" ", String.Empty)
+        Dim OldDigestedExpression As String = String.Empty
+        While DigestedExpression <> OldDigestedExpression
+            OldDigestedExpression = DigestedExpression
+            For Each Operation As String In Operations
+                If Operation = "Pi" Then
+                    For Each Match As Match In Regex.Matches(DigestedExpression, Operation, RegexOptions.IgnoreCase)
+                        DigestedExpression = DigestedExpression.Replace(Match.Value, CalculateOperation(Operation).ToString())
+                    Next
+                ElseIf Operation = "*" Then
+                    For Each Match As Match In Regex.Matches(DigestedExpression, Pattern + "\*" + Pattern, RegexOptions.IgnoreCase)
+                        DigestedExpression = DigestedExpression.Replace(Match.Value, "+" + CalculateExpression(Match.Value))
+                    Next
+                ElseIf Operation = "/" Then
+                    For Each Match As Match In Regex.Matches(DigestedExpression, Pattern + "/" + Pattern, RegexOptions.IgnoreCase)
+                        DigestedExpression = DigestedExpression.Replace(Match.Value, "+" + CalculateExpression(Match.Value))
+                    Next
+                ElseIf Operation = "+" Then
+                    For Each Match As Match In Regex.Matches(DigestedExpression, Pattern + "\+" + Pattern, RegexOptions.IgnoreCase)
+                        DigestedExpression = DigestedExpression.Replace(Match.Value, "+" + CalculateExpression(Match.Value))
+                    Next
+                ElseIf Operation = "-" Then
+                    For Each Match As Match In Regex.Matches(DigestedExpression, Pattern + "-" + Pattern, RegexOptions.IgnoreCase)
+                        DigestedExpression = DigestedExpression.Replace(Match.Value, "+" + CalculateExpression(Match.Value))
+                    Next
+                ElseIf Operation = "Pow" OrElse Operation = "Atan2" Then
+                    For Each GlobalMatch As Match In Regex.Matches(DigestedExpression, Operation + "\(" + Pattern + "," + Pattern + "\)", RegexOptions.IgnoreCase)
+                        For Each NumMatch As Match In Regex.Matches(GlobalMatch.Value, Pattern, RegexOptions.IgnoreCase)
+                            If NumMatch.NextMatch.Value.Trim() = String.Empty Then Continue For
+                            DigestedExpression = DigestedExpression.Replace(GlobalMatch.Value, CalculateOperation(Operation, Convert.ToDouble(NumMatch.Value), Convert.ToDouble(NumMatch.NextMatch().Value)).ToString())
+                        Next
+                    Next
+                Else
+                    For Each GlobalMatch As Match In Regex.Matches(DigestedExpression, Operation + "\(" + Pattern + "\)", RegexOptions.IgnoreCase)
+                        For Each NumMatch As Match In Regex.Matches(GlobalMatch.Value, Pattern, RegexOptions.IgnoreCase)
+                            DigestedExpression = DigestedExpression.Replace(GlobalMatch.Value, CalculateOperation(Operation, Convert.ToDouble(NumMatch.Value)).ToString())
+                        Next
+                    Next
+                End If
+            Next
+        End While
+        Return DigestedExpression
+    End Function
+
+    Function CalculateOperation(Operation As String, Optional X As Double = 1, Optional Y As Double = 1) As Double
+        Select Case Operation
+            Case "+"
+                Return X + Y
+            Case "-"
+                Return X - Y
+            Case "*"
+                Return X * Y
+            Case "/"
+                Return X / Y
+            Case "Abs"
+                Return Math.Abs(X)
+            Case "Acos"
+                Return Math.Acos(X)
+            Case "Acosh"
+                Return Math.Acosh(X)
+            Case "Asin"
+                Return Math.Asin(X)
+            Case "Asinh"
+                Return Math.Asinh(X)
+            Case "Atan"
+                Return Math.Atan(X)
+            Case "Atan2"
+                Return Math.Atan2(X, Y)
+            Case "Atanh"
+                Return Math.Atanh(X)
+            Case "Cbrt"
+                Return Math.Cbrt(X)
+            Case "Ceil"
+                Return Math.Ceiling(X)
+            Case "Cos"
+                Return Math.Cos(X)
+            Case "Cosh"
+                Return Math.Cosh(X)
+            Case "Exp"
+                Return Math.Exp(X)
+            Case "Floor"
+                Return Math.Floor(X)
+            Case "Log", "Ln"
+                Return Math.Log(X)
+            Case "Log10"
+                Return Math.Log10(X)
+            Case "Log2"
+                Return Math.Log2(X)
+            Case "Pi"
+                Return Math.PI
+            Case "Pow"
+                Return Math.Pow(X, Y)
+            Case "Sign"
+                Return Math.Sign(X)
+            Case "Sin"
+                Return Math.Sin(X)
+            Case "Sinh"
+                Return Math.Sinh(X)
+            Case "Sqrt"
+                Return Math.Sqrt(X)
+            Case "Tan"
+                Return Math.Tan(X)
+            Case "Tanh"
+                Return Math.Tanh(X)
+            Case Else
+                Return 0
+        End Select
+    End Function
+
+    Function CalculateExpression(Expression As String) As String
+        Try
+            Dim Result As String = New DataTable().Compute(Expression, Nothing).ToString()
+            Return Result
+        Catch
+            Return "Invalid expression"
+        End Try
+    End Function
+
+    Sub Main(Args As String())
+        CultureInfo.DefaultThreadCurrentCulture = New CultureInfo("en-US")
+        CultureInfo.DefaultThreadCurrentUICulture = New CultureInfo("en-US")
+        If Args.Length = 1 Then
+            Console.WriteLine(CalculateExpression(Digest(Args(0))))
+            Return
+        End If
+        Console.WriteLine("Expression Calculator v1.0.0")
+        While True
+            Console.Write("> ")
+            Console.WriteLine(CalculateExpression(Digest(Console.ReadLine())))
+        End While
+    End Sub
+
+End Module
